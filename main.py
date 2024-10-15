9409091d61b8b56f416b4425d5ae60f6d81fb4be
LoreviQ <lorevi@protonmail.com>
2024-10-15 02:29:53 +0100

    Added section to get time between messages

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ class Chatbot:
 
         # Initialize chat
         self.chat = []
+        self.phase = "INTRODUCTION_PHASE"
         self.set_system_message("INTRODUCTION_PHASE")
         self.chat.append(
             {
@@ -35,13 +36,19 @@ class Chatbot:
             }
         )
 
-    def set_system_message(self, phase):
+    def set_system_message(self, system_message_type):
+        initial_section = ""
+        match system_message_type:
+            case "chat_message":
+                initial_section = f"""
+You are an expert actor that can fully immerse yourself into any role given. You do not break character for any reason, even if someone tries addressing you as an AI or language model.
+Currently your role is {self.character_info['char']}, which is described in detail below. As {self.character_info['char']}, continue the exchange with the user.
+"""
+
         system_message = {
             "role": "system",
             "content": f"""
-You are an expert actor that can fully immerse yourself into any role given. You do not break character for any reason, even if someone tries addressing you as an AI or language model.
-
-Currently your role is {self.character_info['char']}, which is described in detail below. As {self.character_info['char']}, continue the exchange with the user.
+{initial_section}
 Description: {self.character_info['description']}
 Age: {self.character_info['age']}
 Personality: {self.character_info['personality']}
@@ -51,7 +58,9 @@ Hates: {self.character_info['hates']}
 Details: {self.character_info['details']}
 
 The story follows the scenario: {self.character_info['scenario']}
-The story progresses in phases. The currently active phase is {phase} described by: {self.character_info['phases'][phase]}
+The story progresses in phases. The currently active phase is {self.phase} described by: {self.character_info['phases'][self.phase]}
+
+The following is highly important to remember: {self.character_info['important']}
 """,
         }
         if self.chat:
@@ -59,17 +68,24 @@ The story progresses in phases. The currently active phase is {phase} described 
         else:
             self.chat.append(system_message)
 
-    def add_user_input(self, user_input):
-        self.chat.append({"role": "user", "content": user_input})
-
-    def add_response(self, max_new_tokens=512):
+    def get_response(self, next_message, max_new_tokens=512):
+        self.chat.append(next_message)
         response = self.pipe(self.chat, max_new_tokens=max_new_tokens)
-        self.chat.append(
-            {
-                "role": "assistant",
-                "content": response[0]["generated_text"][-1]["content"],
-            }
-        )
+        self.chat = response[0]["generated_text"]
+
+    def check_time(self, next_message, max_new_tokens=32):
+        message = {
+            "role": "user",
+            "content": f"""
+{{user}} is sending you the message:
+{next_message["content"]}
+How long will you take to respond to it? Reply with a duration in the format of number followed by a time unit (e.g., 12h for 12 hours, 15s for 15 seconds, or 2h 20m for 2 hours and 20 minutes).
+""",
+        }
+        temp_chat = self.chat.copy()
+        temp_chat.append(message)
+        response = self.pipe(temp_chat, max_new_tokens=max_new_tokens)
+        print("Time taken to respond:", response[0]["generated_text"][-1]["content"])
 
     def handle_input(self, user_input):
         match user_input.lower():
@@ -77,8 +93,12 @@ The story progresses in phases. The currently active phase is {phase} described 
                 print("Ending the session. Goodbye!")
                 return False
             case _:
-                self.add_user_input(user_input)
-                self.add_response()
+                next_message = {
+                    "role": "user",
+                    "content": user_input,
+                }
+                self.check_time(next_message)
+                self.get_response(next_message)
                 print(self.chat[-1]["content"])
                 return True
 
