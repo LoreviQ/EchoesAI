f2365f68e2ec2a5495e108a0b246a24ba2716940
LoreviQ <lorevi@protonmail.com>
2024-10-17 02:02:50 +0100

    created function to insert row into db

--- database.py
+++ database.py
@@ -1,7 +1,16 @@
+"""
+This module contains the class to manage the database and other database-related functions.
+"""
+
 import sqlite3
+from typing import Dict
 
 
 class DB:
+    """
+    Class to manage the database.
+    """
+
     def __init__(self) -> None:
         # Connect to the database
         self.conn = sqlite3.connect("database.db")
@@ -11,6 +20,19 @@ class DB:
         with open("./sql/schema.sql", "r", encoding="utf-8") as file:
             schema = file.read()
         self.cursor.executescript(schema)
+        self.conn.commit()
+        self.queries: Dict[str, str] = {
+            "post_message": "INSERT INTO messages (user, chatbot, content) VALUES (?, ?, ?)"
+        }
+
+    def post_message(self, user: str, chatbot: str, content: str) -> None:
+        """
+        Insert a message into the database.
+        """
+        self.cursor.execute(
+            self.queries["post_message"],
+            (user, chatbot, content),
+        )
         self.conn.commit()
 
 
