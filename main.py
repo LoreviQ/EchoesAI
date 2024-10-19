d80bf74b98556cab4206a93d4167108a19b0e512
LoreviQ <lorevi@protonmail.com>
2024-10-19 20:29:27 +0100

    Added debug arg to entrypoint

--- main.py
+++ main.py
@@ -2,9 +2,22 @@
 This is the main file that runs the application.
 """
 
+import argparse
+
 from app import App
 
-if __name__ == "__main__":
+
+def main() -> None:
+    """
+    Entry point for the application.
+    """
+    parser = argparse.ArgumentParser(description="Run the application.")
+    parser.add_argument("--debug", action="store_true", help="Enable debug mode")
+    args = parser.parse_args()
     app = App("database.db")
-    app.load_model(True)
+    app.load_model(args.debug)
     app.serve()
+
+
+if __name__ == "__main__":
+    main()
