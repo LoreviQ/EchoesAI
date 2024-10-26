f25afb4f47e5b4bde4266f1081a97b15b1523ff4
LoreviQ <lorevi@protonmail.com>
2024-10-26 16:50:38 +0100

    refactored test_passwords to use defined fixtures

--- tests/test_auth/test_passwords.py
+++ tests/test_auth/test_passwords.py
@@ -2,38 +2,17 @@
 This file contains the tests for the auth/passwords.py file.
 """
 
-import os
-from typing import Generator
+# pylint: disable=redefined-outer-name unused-argument unused-import
 
 import pytest
 
 import auth
 import database as db
-
-# pylint: disable=redefined-outer-name unused-argument unused-import
-
-
-@pytest.fixture
-def user(
-    monkeypatch: pytest.MonkeyPatch,
-) -> Generator[db.User, None, None]:
-    """
-    Create a DB object for testing and teardown after testing.
-    """
-    test_name = os.environ.get("PYTEST_CURRENT_TEST")
-    if test_name is None:
-        test_name = "unknown"
-    else:
-        test_name = test_name.split(":")[-1].split(" ")[0]
-    db_path = f"test_database_{test_name}.db"
-    monkeypatch.setattr("database.main.DB_PATH", db_path)
-    db.create_db()
-    user = db.User(username="test_user", password="password", email="test@test.com")
-    yield user
-    os.remove(db_path)
+from tests.test_database.test_main import db_init
+from tests.test_database.test_users import user
 
 
-def test_insert_user(user: db.User) -> None:
+def test_insert_user(db_init: str, user: db.User) -> None:
     """
     Test the insert_user function.
     """
@@ -46,7 +25,7 @@ def test_insert_user(user: db.User) -> None:
     assert user_db["password"] != original_password
 
 
-def test_authenticate_user(user: db.User) -> None:
+def test_authenticate_user(db_init: str, user: db.User) -> None:
     """
     Test the authenticate_user function.
     """
