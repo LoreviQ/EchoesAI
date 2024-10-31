ceeef172fa124fd462929417e5d8327a3c7234e3
LoreviQ <lorevi@protonmail.com>
2024-10-31 22:38:41 +0000

    Adjusted character creation

--- src/routes/characters.py
+++ src/routes/characters.py
@@ -65,46 +65,46 @@ def get_events_by_character(char_path: str) -> Response:
 def _create_character_query_params(query_params: dict[str, str]) -> db.Character:
     character_query = db.Character()
     # basic params
-    if "id" in query_params:
+    if "id" in query_params and query_params["id"]:
         character_query["id"] = int(query_params["id"])
-    if "name" in query_params:
+    if "name" in query_params and query_params["name"]:
         character_query["name"] = query_params["name"]
-    if "path_name" in query_params:
+    if "path_name" in query_params and query_params["path_name"]:
         character_query["path_name"] = query_params["path_name"]
-    if "description" in query_params:
+    if "description" in query_params and query_params["description"]:
         character_query["description"] = query_params["description"]
-    if "age" in query_params:
+    if "age" in query_params and query_params["age"]:
         character_query["age"] = int(query_params["age"])
-    if "height" in query_params:
+    if "height" in query_params and query_params["height"]:
         character_query["height"] = query_params["height"]
-    if "personality" in query_params:
+    if "personality" in query_params and query_params["personality"]:
         character_query["personality"] = query_params["personality"]
-    if "appearance" in query_params:
+    if "appearance" in query_params and query_params["appearance"]:
         character_query["appearance"] = query_params["appearance"]
-    if "loves" in query_params:
+    if "loves" in query_params and query_params["loves"]:
         character_query["loves"] = query_params["loves"]
-    if "hates" in query_params:
+    if "hates" in query_params and query_params["hates"]:
         character_query["hates"] = query_params["hates"]
-    if "details" in query_params:
+    if "details" in query_params and query_params["details"]:
         character_query["details"] = query_params["details"]
-    if "scenario" in query_params:
+    if "scenario" in query_params and query_params["scenario"]:
         character_query["scenario"] = query_params["scenario"]
-    if "important" in query_params:
+    if "important" in query_params and query_params["important"]:
         character_query["important"] = query_params["important"]
-    if "initial_message" in query_params:
+    if "initial_message" in query_params and query_params["initial_message"]:
         character_query["initial_message"] = query_params["initial_message"]
-    if "favorite_colour" in query_params:
+    if "favorite_colour" in query_params and query_params["favorite_colour"]:
         character_query["favorite_colour"] = query_params["favorite_colour"]
-    if "phases" in query_params:
+    if "phases" in query_params and query_params["phases"]:
         character_query["phases"] = bool(query_params["phases"])
-    if "img_gen" in query_params:
+    if "img_gen" in query_params and query_params["img_gen"]:
         character_query["img_gen"] = bool(query_params["img_gen"])
-    if "model" in query_params:
+    if "model" in query_params and query_params["model"]:
         character_query["model"] = query_params["model"]
-    if "global_positive" in query_params:
+    if "global_positive" in query_params and query_params["global_positive"]:
         character_query["global_positive"] = query_params["global_positive"]
-    if "global_negative" in query_params:
+    if "global_negative" in query_params and query_params["global_negative"]:
         character_query["global_negative"] = query_params["global_negative"]
-    if "profile_path" in query_params:
+    if "profile_path" in query_params and query_params["profile_path"]:
         character_query["profile_path"] = query_params["profile_path"]
     return character_query
--- src/test.py
+++
@@ -1,4 +0,0 @@
-from database import create_db
-
-if __name__ == "__main__":
-    create_db()
